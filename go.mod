c64afe91aa262e07e360db7bc430568125c73a8e
Amaury Franssen <amauryfranssen@gmail.com>
2023-10-20 01:36:00 +0200

    bdd initial

--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/go-chi/chi/v5 v5.0.10
 	github.com/go-sql-driver/mysql v1.7.1
 )
+
+require github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -14,5 +19,23 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello My name is Golang"))
 	})
+
+	// Replace these connection parameters with your actual PostgreSQL credentials
+	connStr := "postgres://database_postgre_pn0t_user:0bITMm4I5lLLfFhfvCYkHQtJNcxzHYX3@dpg-ckor2m41tcps73e73qh0-a/database_postgre_pn0t?sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connected to the PostgreSQL database!")
+
+	// Perform your database operations here...
+
 	http.ListenAndServe(":2345", r)
 }
