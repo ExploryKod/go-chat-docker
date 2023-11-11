927e2f57c83dc88c575ea0666c991cc9ef9f8bb5
Amaury Franssen <amauryfranssen@gmail.com>
2023-11-11 15:12:17 +0100

    feat: add login methods and jwt auth package

--- main.go
+++ main.go
@@ -6,13 +6,15 @@ import (
 	"fmt"
 	"github.com/go-chi/cors"
 	"github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
-
-	_ "github.com/lib/pq"
+	"strconv"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/jwtauth/v5"
 )
 
 func CreateStore(db *sql.DB) *Store {
@@ -157,6 +159,86 @@ func (h *Handler) RegisterHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		// Respond with a success message
 		h.jsonResponse(w, http.StatusOK, map[string]interface{}{"message": "Registration successful", "userID": userID})
+	}
+}
+
+var tokenAuth *jwtauth.JWTAuth
+
+const Secret = "mysecretamaury"
+
+func init() {
+	tokenAuth = jwtauth.New("HS256", []byte(Secret), nil)
+}
+
+func MakeToken(name string) string {
+	_, tokenString, _ := tokenAuth.Encode(map[string]interface{}{"username": name})
+	return tokenString
+}
+
+func (h *Handler) LoginHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		// Extract username and password from the request body or form data
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		// Validate user credentials against the database
+		user, err := h.Store.GetUserByUsername(username)
+		if err != nil {
+			// Handle database error
+			h.jsonResponse(w, http.StatusInternalServerError, map[string]interface{}{
+				"message": "Internal Server Error" + err.Error(),
+			})
+			return
+		}
+
+		if user.Username == "" || user.Password == "" {
+			http.Error(w, "Il reste des champs vide", http.StatusBadRequest)
+			return
+		}
+
+		// Check if the user exists and the password matches
+		if user.Username == username && user.Password == password {
+			token := MakeToken(username)
+
+			http.SetCookie(w, &http.Cookie{
+				HttpOnly: true,
+				Expires:  time.Now().Add(7 * 24 * time.Hour),
+				SameSite: http.SameSiteLaxMode,
+				// Uncomment below for HTTPS:
+				// Secure: true,
+				Name:  "jwt", // Must be named "jwt" or else the token cannot be searched for by jwtauth.Verifier.
+				Value: token,
+			})
+			// Successful login
+
+			// Convert role (admin column) to string
+			var roleStr string
+
+			if user.Admin != nil {
+				roleStr = strconv.Itoa(*user.Admin)
+			} else {
+				roleStr = "0"
+			}
+
+			response := map[string]string{"message": "Vous êtes bien connecté", "redirect": "/", "token": token, "role": roleStr}
+			h.jsonResponse(w, http.StatusOK, response)
+		} else if user.Password != password {
+			// Failed login
+			h.jsonResponse(w, http.StatusUnauthorized, map[string]interface{}{
+				"message": "Mot de passe incorrect",
+			})
+		} else if user.Username != username {
+			// Failed login
+			h.jsonResponse(w, http.StatusUnauthorized, map[string]interface{}{
+				"message": "Nom d'utilisateur incorrect",
+			})
+		} else {
+			// Failed login
+			h.jsonResponse(w, http.StatusUnauthorized, map[string]interface{}{
+				"message": "Nom d'utilisateur et mot de passe incorrects",
+			})
+		}
 	}
 }
 
