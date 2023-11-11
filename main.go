9b223455e5014bbee12076f2a72538a94e4aee27
Amaury Franssen <amauryfranssen@gmail.com>
2023-11-11 20:30:33 +0100

    first separation in a flat architecture

--- main.go
+++ main.go
@@ -4,32 +4,18 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 	"github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
 	"log"
 	"net/http"
-	"strconv"
-	"time"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/jwtauth/v5"
 )
 
 func CreateStore(db *sql.DB) *Store {
 	return &Store{
 		NewUserStore(db),
-	}
-}
-
-type UserStore struct {
-	*sql.DB
-}
-
-func NewUserStore(db *sql.DB) *UserStore {
-	return &UserStore{
-		db,
 	}
 }
 
@@ -42,7 +28,10 @@ type UserItem struct {
 
 type UserStoreInterface interface {
 	AddUser(item UserItem) (int, error)
+	GetUsers() ([]UserItem, error)
 	GetUserByUsername(username string) (UserItem, error)
+	DeleteUserById(id int) error
+	UpdateUser(item UserItem) error
 }
 
 type Store struct {
@@ -105,142 +94,7 @@ func main() {
 	handler.Post("/auth/register", handler.RegisterHandler)
 	handler.Post("/auth/logged", handler.LoginHandler())
 
-	// Add a new user
-
 	http.ListenAndServe(":2345", handler)
-}
-
-// DATABASE OPERATION
-func (t *UserStore) AddUser(item UserItem) (int, error) {
-	res, err := t.DB.Exec("INSERT INTO Users (username, password) VALUES (?, ?)", item.Username, item.Password)
-	if err != nil {
-		return 0, err
-	}
-
-	id, err := res.LastInsertId()
-	if err != nil {
-		return 0, err
-	}
-
-	return int(id), nil
-}
-
-func (t *UserStore) GetUserByUsername(username string) (UserItem, error) {
-	var user UserItem
-
-	err := t.QueryRow("SELECT id, username, password FROM Users WHERE username = ?", username).
-		Scan(&user.ID, &user.Username, &user.Password)
-
-	if err == sql.ErrNoRows {
-		// User not found
-		return UserItem{}, nil
-	} else if err != nil {
-		// Handle other database errors
-		return UserItem{}, err
-	}
-
-	return user, nil
-}
-
-func (h *Handler) RegisterHandler(w http.ResponseWriter, r *http.Request) {
-	// Extract registration data
-	username := r.FormValue("username")
-	password := r.FormValue("password")
-	existentUser, err := h.Store.GetUserByUsername(username)
-	if err != nil {
-
-		h.jsonResponse(w, http.StatusBadRequest, map[string]interface{}{"message": "L'utilisateur existe déjà", "code": http.StatusBadRequest})
-		return
-	} else if existentUser.Username != username {
-		userID, err := h.Store.AddUser(UserItem{Username: username, Password: password})
-		if err != nil {
-			//http.Error(w, err.Error(), http.StatusInternalServerError)
-			h.jsonResponse(w, http.StatusInternalServerError, map[string]interface{}{"message": "l'utilisateur n'a pu être ajouté"})
-			return
-		}
-		// Respond with a success message
-		h.jsonResponse(w, http.StatusOK, map[string]interface{}{"message": "Registration successful", "userID": userID})
-	}
-}
-
-var tokenAuth *jwtauth.JWTAuth
-
-const Secret = "mysecretamaury"
-
-func init() {
-	tokenAuth = jwtauth.New("HS256", []byte(Secret), nil)
-}
-
-func MakeToken(name string) string {
-	_, tokenString, _ := tokenAuth.Encode(map[string]interface{}{"username": name})
-	return tokenString
-}
-
-func (h *Handler) LoginHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-
-		// Extract username and password from the request body or form data
-		username := r.FormValue("username")
-		password := r.FormValue("password")
-
-		// Validate user credentials against the database
-		user, err := h.Store.GetUserByUsername(username)
-		if err != nil {
-			// Handle database error
-			h.jsonResponse(w, http.StatusInternalServerError, map[string]interface{}{
-				"message": "Internal Server Error" + err.Error(),
-			})
-			return
-		}
-
-		if user.Username == "" || user.Password == "" {
-			http.Error(w, "Il reste des champs vide", http.StatusBadRequest)
-			return
-		}
-
-		// Check if the user exists and the password matches
-		if user.Username == username && user.Password == password {
-			token := MakeToken(username)
-
-			http.SetCookie(w, &http.Cookie{
-				HttpOnly: true,
-				Expires:  time.Now().Add(7 * 24 * time.Hour),
-				SameSite: http.SameSiteLaxMode,
-				// Uncomment below for HTTPS:
-				// Secure: true,
-				Name:  "jwt", // Must be named "jwt" or else the token cannot be searched for by jwtauth.Verifier.
-				Value: token,
-			})
-			// Successful login
-
-			// Convert role (admin column) to string
-			var roleStr string
-
-			if user.Admin != nil {
-				roleStr = strconv.Itoa(*user.Admin)
-			} else {
-				roleStr = "0"
-			}
-
-			response := map[string]string{"message": "Vous êtes bien connecté", "redirect": "/", "token": token, "role": roleStr}
-			h.jsonResponse(w, http.StatusOK, response)
-		} else if user.Password != password {
-			// Failed login
-			h.jsonResponse(w, http.StatusUnauthorized, map[string]interface{}{
-				"message": "Mot de passe incorrect",
-			})
-		} else if user.Username != username {
-			// Failed login
-			h.jsonResponse(w, http.StatusUnauthorized, map[string]interface{}{
-				"message": "Nom d'utilisateur incorrect",
-			})
-		} else {
-			// Failed login
-			h.jsonResponse(w, http.StatusUnauthorized, map[string]interface{}{
-				"message": "Nom d'utilisateur et mot de passe incorrects",
-			})
-		}
-	}
 }
 
 func (h *Handler) jsonResponse(w http.ResponseWriter, status int, data interface{}) {
---
+++ user_store.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"database/sql"
+)
+
+func NewUserStore(db *sql.DB) *UserStore {
+	return &UserStore{
+		db,
+	}
+}
+
+type UserStore struct {
+	*sql.DB
+}
+
+func (t *UserStore) GetUsers() ([]UserItem, error) {
+	var users []UserItem
+
+	rows, err := t.Query("SELECT id, username, password, admin FROM Users")
+	if err != nil {
+		return []UserItem{}, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var user UserItem
+		if err = rows.Scan(&user.ID, &user.Username, &user.Password, &user.Admin); err != nil {
+			return []UserItem{}, err
+		}
+		users = append(users, user)
+	}
+
+	if err = rows.Err(); err != nil {
+		return []UserItem{}, err
+	}
+
+	return users, nil
+}
+
+func (t *UserStore) GetUserByUsername(username string) (UserItem, error) {
+	var user UserItem
+
+	err := t.QueryRow("SELECT id, username, password FROM Users WHERE username = ?", username).
+		Scan(&user.ID, &user.Username, &user.Password)
+
+	if err == sql.ErrNoRows {
+		// User not found
+		return UserItem{}, nil
+	} else if err != nil {
+		// Handle other database errors
+		return UserItem{}, err
+	}
+
+	return user, nil
+}
+
+func (t *UserStore) AddUser(item UserItem) (int, error) {
+	res, err := t.DB.Exec("INSERT INTO Users (username, password) VALUES (?, ?)", item.Username, item.Password)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
+}
+
+func (t *UserStore) UpdateUser(item UserItem) error {
+
+	_, err := t.DB.Exec("UPDATE Users SET username = ?, password = ?, admin = ? WHERE id = ?", item.Username, item.Password, item.Admin, item.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func (t *UserStore) DeleteUserById(id int) error {
+	_, err := t.DB.Exec("DELETE FROM Users WHERE id = ?", id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
